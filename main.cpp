cccf03a156557423c71090a5fd0e31e6926e500d
seohyeong <seohyeong.j@sk.com>
2024-02-13 10:10:44 +0900

    smoothlife gliders

--- main.cpp
+++ main.cpp
@@ -7,27 +7,25 @@
 #include <raylib.h>
 
 
-#define BOARD_SIZE 400
+#define BOARD_SIZE 500
 #define CELL_SIZE 1
 
 #define ROW BOARD_SIZE / CELL_SIZE
 #define COL BOARD_SIZE / CELL_SIZE
 
-// #define ROW 20
-// #define COL 20
-
-#define SHBLACK {31,31,31,255}
+// #define ROW 50
+// #define COL 100
 
 // smoothlife parameters
-int r_a = 12;
-int r_i = 3;
+int r_a = 6;
+int r_i = 2;
 float b_1 = 0.278;
 float b_2 = 0.365;
 float d_1 = 0.267;
 float d_2 = 0.445;
 float alpha_n = 0.028;
 float alpha_m = 0.147;
-float d_t = 0.05f;
+float d_t = 0.1f;
 
 
 // less dense to more dense
@@ -43,8 +41,8 @@ struct State{
 
 
 void init_cells(State& state){
-	for(int i=0; i<ROW; i++){
-		for(int j=0; j<COL; j++){
+	for(int i=0; i<ROW/2; i++){
+		for(int j=0; j<COL/2; j++){
 			state.current_cells[i][j] = (float)rand()/(float)RAND_MAX;
 		}
 	}
@@ -80,6 +78,7 @@ int mod(int x, int y){
 }
 
 
+// ~O(n^3)
 void integral_of(State& state, int i, int j, float array[2]){
 	int M = 0;
 	int N = 0;
@@ -90,10 +89,10 @@ void integral_of(State& state, int i, int j, float array[2]){
 		for(int jj=j-r_a; jj<=j+r_a; jj++){
 			int d_i_pow = (i-ii)*(i-ii);
 			int d_j_pow = (j-jj)*(j-jj);
-			if(d_i_pow + d_j_pow < r_i*r_i){
+			if(d_i_pow + d_j_pow <= r_i*r_i){
 				m += state.current_cells[mod(ii, ROW)][mod(jj, COL)];
 				M += 1;
-			} else if(d_i_pow + d_j_pow < r_a*r_a){
+			} else if(d_i_pow + d_j_pow <= r_a*r_a){
 				n += state.current_cells[mod(ii, ROW)][mod(jj, COL)];
 				N += 1;
 			}
@@ -139,10 +138,16 @@ void get_next(State& state){
 			float n = m_and_n[1];
 
 			// calculate S(n, m)
-			float cell_diff = 2*s(n, m) - 1;
+			// float cell_diff = 2*s(n, m) - 1;
+
 			// TODO: this has to be inbetween 0 and 1
-			state.next_cells[i][j] = state.current_cells[i][j] + d_t * cell_diff;
-			if(state.next_cells[i][j] < 0){state.next_cells[i][j] = 0.0f;}
+			// state.next_cells[i][j] = state.current_cells[i][j] + d_t * cell_diff;
+			state.next_cells[i][j] = s(n, m);
+			// if(state.next_cells[i][j] < 0){
+			// 	state.next_cells[i][j] = 0.0f;
+			// } else if(state.next_cells[i][j] > 1){
+			// 	state.next_cells[i][j] = 1.0f;
+			// }
 
 			// // discrete time stepping
 			// state.next_cells[i][j] = s(n, m);
